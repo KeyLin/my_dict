eebf23cd498faad565495760fd7d5cbaf00fc91b
killin <cailin@live.cn>
2014-11-11 10:55:07 +0800

    multithread

---
+++ multiThread.py
@@ -0,0 +1,15 @@
+import pydict
+import time
+import threading
+
+if __name__ == '__main__':
+    name = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+    dic = pydict.Pydict(name+'.txt',True,True)
+    p1 = threading.Thread(target=dic.monitorClip)
+    p2 = threading.Thread(target=dic.input)
+    p1.start()
+    p2.start()
+    dic.resolve()
+    p1.join()
+    p2.join()
+    print 'Thread end.'
